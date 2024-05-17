d6ccdbe7bf513629a9e32b14442e442cb98caf76
Asmi <asmigulati9@gmail.com>
2024-05-17 22:28:19 +0530

    m

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -14,115 +14,117 @@ import { cn } from '@/lib/utils'
 import { toast } from 'sonner'
 
 export interface ChatPanelProps {
-  id?: string
-  title?: string
-  input: string
-  setInput: (value: string) => void
-  isAtBottom: boolean
-  scrollToBottom: () => void
+    id?: string
+    title?: string
+    input: string
+    setInput: (value: string) => void
+    isAtBottom: boolean
+    scrollToBottom: () => void
 }
 
 export function ChatPanel({
-                            id,
-                            title,
-                            input,
-                            setInput,
-                            isAtBottom,
-                            scrollToBottom
-                          }: ChatPanelProps) {
-  const [aiState] = useAIState()
-  const [messages, setMessages] = useUIState<typeof AI>()
-  const { submitUserMessage } = useActions()
-  const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
-  const [showMessage, setShowMessage] = React.useState(true)
-
-  const exampleMessages = [
-    {
-      heading: 'Brainstorm a concept',
-      subheading: '',
-      message: ``
-    },
-    {
-      heading: 'Analyze a dataset',
-      subheading: '',
-      message: ''
-    },
-      {
-          heading: 'Summarise an Article',
-          subheading: '',
-          message: ''
-      }
-  ]
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowMessage(false)
-    }, 5000) // Message disappears after 5 seconds
-    return () => clearTimeout(timer)
-  }, [])
-
-  return (
-      <div className="fixed inset-x-0 bg-white/90 bottom-0 w-full duration-300 ease-in-out peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px] dark:from-10%">
-        <ButtonScrollToBottom
-            isAtBottom={isAtBottom}
-            scrollToBottom={scrollToBottom}
-        />
-
-        <div className="mx-auto sm:max-w-2xl sm:px-4">
-            <img className="size-10 object-contain ml-auto mr-auto mb-10" src="/images/gemini.png" alt="gemini logo" />
-          <div className="mb-24 grid sm:grid-cols-3 gap-2 sm:gap-4 px-4 sm:px-0">
-            {messages.length === 0 &&
-                exampleMessages.map((example, index) => (
-                    <div
-                        key={example.heading}
-                        className={cn(
-                            'cursor-pointer bg-zinc-50 text-zinc-950 rounded-2xl p-4 sm:p-6 hover:bg-zinc-100 transition-colors',
-                            index > 1 && 'hidden md:block'
-                        )}
-                    >
-                      <div className="font-medium">{example.heading}</div>
-                      <div className="text-sm text-zinc-800">
-                        {example.subheading}
-                      </div>
-                    </div>
-                ))}
-          </div>
-
-
-          {messages?.length >= 2 ? (
-              <div className="flex h-fit items-center justify-center">
-                <div className="flex space-x-2">
-                  {id && title ? (
-                      <>
-                        <Button
-                            variant="outline"
-                            onClick={() => setShareDialogOpen(true)}
-                        >
-                          <IconShare className="mr-2" />
-                          Share
-                        </Button>
-                        <ChatShareDialog
-                            open={shareDialogOpen}
-                            onOpenChange={setShareDialogOpen}
-                            onCopy={() => setShareDialogOpen(false)}
-                            shareChat={shareChat}
-                            chat={{
                               id,
                               title,
-                              messages: aiState.messages
-                            }}
-                        />
-                      </>
-                  ) : null}
-                </div>
-              </div>
-          ) : null}
+                              input,
+                              setInput,
+                              isAtBottom,
+                              scrollToBottom
+                          }: ChatPanelProps) {
+    const [aiState] = useAIState()
+    const [messages, setMessages] = useUIState<typeof AI>()
+    const { submitUserMessage } = useActions()
+    const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
+    const [showContent, setShowContent] = React.useState(true)
 
-          <div className="grid gap-4 sm:pb-4">
-            <PromptForm input={input} setInput={setInput} />
-            <FooterText className="hidden sm:block" />
-          </div>
+    const exampleMessages = [
+        {
+            heading: 'Brainstorm a concept',
+            subheading: '',
+            message: ``
+        },
+        {
+            heading: 'Analyze a dataset',
+            subheading: '',
+            message: ''
+        },
+        {
+            heading: 'Summarise an Article',
+            subheading: '',
+            message: ''
+        }
+    ]
+
+    React.useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowContent(false)
+        }, 5000) // Content disappears after 5 seconds
+        return () => clearTimeout(timer)
+    }, [])
+
+    return (
+        <div className="fixed inset-x-0 bg-white/90 bottom-0 w-full duration-300 ease-in-out peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px] dark:from-10%">
+            <ButtonScrollToBottom
+                isAtBottom={isAtBottom}
+                scrollToBottom={scrollToBottom}
+            />
+
+            <div className="mx-auto sm:max-w-2xl sm:px-4">
+                {showContent && (
+                    <img className="size-10 object-contain ml-auto mr-auto mb-10" src="/images/gemini.png" alt="gemini logo" />
+                )}
+                {messages.length === 0 && showContent && (
+                    <div className="mb-24 grid sm:grid-cols-3 gap-2 sm:gap-4 px-4 sm:px-0">
+                        {exampleMessages.map((example, index) => (
+                            <div
+                                key={example.heading}
+                                className={cn(
+                                    'cursor-pointer bg-zinc-50 text-zinc-950 rounded-2xl p-4 sm:p-6 hover:bg-zinc-100 transition-colors',
+                                    index > 1 && 'hidden md:block'
+                                )}
+                            >
+                                <div className="font-medium">{example.heading}</div>
+                                <div className="text-sm text-zinc-800">
+                                    {example.subheading}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+                {messages?.length >= 2 ? (
+                    <div className="flex h-fit items-center justify-center">
+                        <div className="flex space-x-2">
+                            {id && title ? (
+                                <>
+                                    <Button
+                                        variant="outline"
+                                        onClick={() => setShareDialogOpen(true)}
+                                    >
+                                        <IconShare className="mr-2" />
+                                        Share
+                                    </Button>
+                                    <ChatShareDialog
+                                        open={shareDialogOpen}
+                                        onOpenChange={setShareDialogOpen}
+                                        onCopy={() => setShareDialogOpen(false)}
+                                        shareChat={shareChat}
+                                        chat={{
+                                            id,
+                                            title,
+                                            messages: aiState.messages
+                                        }}
+                                    />
+                                </>
+                            ) : null}
+                        </div>
+                    </div>
+                ) : null}
+
+                <div className="grid gap-4 sm:pb-4">
+                    <PromptForm input={input} setInput={setInput} />
+                    <FooterText className="hidden sm:block" />
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
